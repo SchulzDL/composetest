885d5da0104ac1cdc710b7850252d466419c6e06
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-17 13:30:46 -0700

    had a syntax error in the function defition had a semi colon instead of a colon

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redit(host='redis', port=6379)
 
-def get_hit_count();
+def get_hit_count():
     retries = 5
     while True:
         try:
